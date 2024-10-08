a12a764a26e1e69cc903d7fa8ddd0c00d7445859
mauricio <mauricioferro9304@gmail.com>
2024-10-08 16:56:24 -0300

    teste de comit para arquivo .env e demais

--- db.js
+++ db.js
@@ -1,18 +1,23 @@
-const mysql = require('mysql2');
+require("dotenv").config();
+const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-    host: "sql.freedb.tech",
-    user: "freedb_usuario123",
-    password: "Bx!cb572xphR@u#",
-    database: "freedb_siste"
+const conexaoDB = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  connectTimeout: process.env.DB_TIMEOUT
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err);
-        return;
-    }
-    console.log('Conectado ao banco de dados MySQL!');
+conexaoDB.connect((err) => {
+  if (err) {
+    console.error("Erro ao conectar ao banco de dados:", err);
+    return;
+  }
+  console.log(
+    "Conectado ao banco de dados MySQL porta: " + process.env.DB_PORT
+  );
 });
 
-module.exports = db;
+module.exports = conexaoDB;
